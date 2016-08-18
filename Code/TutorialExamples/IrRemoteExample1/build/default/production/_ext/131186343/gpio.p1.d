d0c48e42f7bc03db830e3ced99adb23c79c5c810
SaheblalBagwan <bagwan@exploreembedded.com>
2016-08-18 10:25:48 +0530

    .

--- Code/TutorialExamples/IrRemoteExample1/build/default/production/_ext/131186343/gpio.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/_ext/131186343/gpio.d  \
- build/default/production/_ext/131186343/gpio.p1:  \
- ../../SampleCode/00-libfiles/gpio.c  \
-../../SampleCode/00-libfiles/stdutils.h  \
-../../SampleCode/00-libfiles/gpio.h 
--- Code/TutorialExamples/IrRemoteExample1/build/default/production/main.p1.d
+++
@@ -1,5 +0,0 @@
- build/default/production/main.d  \
- build/default/production/main.p1:  \
- main.c  \
-../../SampleCode/00-libfiles/uart.h  \
-../../SampleCode/00-libfiles/stdutils.h 
